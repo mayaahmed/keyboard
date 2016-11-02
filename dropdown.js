f974a622be6efa58e1853397643dee73ad61cd47
Maya Mohsin Ahmed <maya.ahmed@gmail.com>
2016-11-02 10:11:17 +0530

    Add files via upload

--- dropdown.js
+++ dropdown.js
@@ -10,3 +10,23 @@ function toggleCP(){
 		cp.style.left = "-1200px";
 	}
 }
+
+
+
+
+
+
+
+
+function slideOpen(el){
+el.style= "-webkit-transition: width 0.5s ease-in 0s";
+el.style.width="300px";
+el.style.visibility="visible";
+}
+
+function slideClose(el){
+el.style.transition="width 1.0s linear 0s";
+el.style.width="0px";
+el.style.border="none";
+}
+
